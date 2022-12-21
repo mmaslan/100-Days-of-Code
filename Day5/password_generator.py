e5160fcb08691943524cbb13aef20e87e604f6d8
mmaslan <marek.maslany1@gmail.com>
2022-12-21 22:55:38 +0100

    Day 5 - Password Generator

---
+++ Day5/password_generator.py
@@ -0,0 +1,33 @@
+
+import random
+import string
+
+letters = list(string.ascii_lowercase + string.ascii_lowercase.upper())
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to PyPassword Generator!")
+
+number_of_letters = int(input("How many letters would you like in your password?\n"))
+number_of_numbers = int(input("How many numbers would you like?\n"))
+number_of_symbols = int(input("How many symbols would you like?\n"))
+
+password = ''
+password_list = []
+
+for char in range(1, number_of_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, number_of_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+for char in range(1, number_of_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+random.shuffle(password_list)
+
+for char in password_list:
+    password += char
+
+message = f"Here is your password: {password}"
+print(message)
