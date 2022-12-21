29e39f76fad64d8dd61eb775b60860288cfb136f
mmaslan <marek.maslany1@gmail.com>
2022-12-21 23:26:21 +0100

    Update

--- Day5/password_generator.py
+++ Day5/password_generator.py
@@ -2,7 +2,7 @@
 import random
 import string
 
-letters = list(string.ascii_lowercase + string.ascii_lowercase.upper())
+letters = list(string.ascii_lowercase + string.ascii_uppercase)
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
