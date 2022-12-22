8325c5fd68556c85a4edf09ee4f4c8aa5ed22688
mmaslan <marek.maslany1@gmail.com>
2022-12-22 23:56:58 +0100

    Day 6 - Code for Reeborg's Worlds

---
+++ Day6/reeborgs_world.py
@@ -0,0 +1,22 @@
+
+# Code for Reeborg's Worlds
+
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+while front_is_clear():
+    move()
+turn_left():
+
+while not_at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
