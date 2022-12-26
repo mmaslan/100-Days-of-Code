20fb2516fdb8afb8eaba11c5550499f3e5332965
mmaslan <marek.maslany1@gmail.com>
2022-12-26 22:33:22 +0100

    Day 10

---
+++ Day10/calculator.py
@@ -0,0 +1,30 @@
+
+from replit import clear
+from calculator_art import logo
+
+
+def add(n1, n2)
+    return n1 + n2
+
+
+def subtract(n1, n2):
+  return n1 - n2
+
+
+def multiply(n1, n2):
+  return n1 * n2
+
+
+def divide(n1, n2):
+  return n1 / n2
+
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+def calculator():
+  print(logo)
