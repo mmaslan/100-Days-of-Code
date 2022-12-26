93745f99c7344b19b0e84e1b8f8a2ce70afecdce
mmaslan <marek.maslany1@gmail.com>
2022-12-26 20:51:08 +0100

    Update

--- Day9/blind_auction.py
+++ Day9/blind_auction.py
@@ -1,4 +1,3 @@
-
 from replit import clear
 from blind_auction_art import logo
 
@@ -9,13 +8,26 @@ print("Welcome to secret auction program!")
 bids = {}
 bidding_finished = False
 
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
 while not bidding_finished:
-    name = input("What is your name? ")
-    price = input("What is your bid? $")
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
     bids[name] = price
-    should_continue = str(input("Are there other bidders? Type: 'yes' or 'no'"))
+    should_continue = input("Are there other bidders? Type: 'yes' or 'no'.\n")
     if should_continue == "no":
         bidding_finished = True
+        find_highest_bidder(bids)
     elif should_continue == "yes":
         clear()
     else:
