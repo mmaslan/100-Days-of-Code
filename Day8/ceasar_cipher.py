92f5d8a15807017ea017b9893b38f6584462a9b5
mmaslan <marek.maslany1@gmail.com>
2022-12-26 15:04:08 +0100

    Update

--- Day8/ceasar_cipher.py
+++ Day8/ceasar_cipher.py
@@ -1,8 +1,7 @@
-from art import logo
+from ceasar_cipher_art import logo
+import string
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
-            'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-            'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet = letters = list(string.ascii_letters)
 
 
 def caesar(start_text, shift_amount, cipher_direction):
