55e5f0a3353720cbfe59b24325a7a001192fd821
mmaslan <marek.maslany1@gmail.com>
2022-12-28 22:19:33 +0100

    Update

--- Day12/number_guessing_game.py
+++ Day12/number_guessing_game.py
@@ -1,4 +1,5 @@
 
+import random
 from random import randint
 from number_guessing_game_art import logo
 
@@ -6,13 +7,33 @@ EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
 
-def numbers():
+def roll_the_numbers():
     list_of_numbers = []
     for i in range(1, 101):
         list_of_numbers.append(i)
+    random_number = random.choice(list_of_numbers)
+    return random_number
 
-    print(list_of_numbers)
+
+def compare():
+    pass
 
 
-print("I'm thinking of number between 1 and 100")
-difficulty = input("Choose a difficulty: 'easy' or 'hard.")
+def the_game():
+
+    print(logo)
+
+    user_number = []
+    computer_number = []
+    turns = 0
+    is_game_over = False
+
+    print("I'm thinking of number between 1 and 100")
+    difficulty = input("Choose a difficulty: 'easy' or 'hard.")
+    if difficulty.lower == 'easy':
+        turns = EASY_LEVEL_TURNS
+    elif difficulty.lower == 'hard':
+        turns = HARD_LEVEL_TURNS
+
+    user_number.append(roll_the_numbers)
+    computer_number .append(roll_the_numbers)
