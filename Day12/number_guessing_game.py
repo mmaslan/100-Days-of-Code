f4b693d51a95c5260cbeed6e76c770a59a529c7e
mmaslan <marek.maslany1@gmail.com>
2022-12-28 23:05:23 +0100

    Update

--- Day12/number_guessing_game.py
+++ Day12/number_guessing_game.py
@@ -1,5 +1,4 @@
 
-import random
 from random import randint
 from number_guessing_game_art import logo
 
@@ -7,33 +6,36 @@ EASY_LEVEL_TURNS = 10
 HARD_LEVEL_TURNS = 5
 
 
-def roll_the_numbers():
-    list_of_numbers = []
-    for i in range(1, 101):
-        list_of_numbers.append(i)
-    random_number = random.choice(list_of_numbers)
-    return random_number
+def compare(guess, answer, turns):
 
-
-def compare():
-    pass
+    if guess == answer:
+        return turns - 1
+    if guess > answer:
+        print("Too low.")
+        return turns - 1
+    elif guess < answer:
+        print("Too high.")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {number_to_guess}.")
 
 
 def the_game():
 
     print(logo)
 
-    user_number = []
-    computer_number = []
-    turns = 0
+    answer = randint(1, 100)
     is_game_over = False
 
     print("I'm thinking of number between 1 and 100")
     difficulty = input("Choose a difficulty: 'easy' or 'hard.")
+
     if difficulty.lower == 'easy':
         turns = EASY_LEVEL_TURNS
+        return "You have 10 attempts remaining to guess the number."
     elif difficulty.lower == 'hard':
         turns = HARD_LEVEL_TURNS
+        return "You have 5 attempts remaining to guess the number."
 
-    user_number.append(roll_the_numbers)
-    computer_number .append(roll_the_numbers)
+
+# You've run out of guesses, you lose.
