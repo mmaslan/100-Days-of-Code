e7804f83070b3f3bd08346bf2a3649b543f0c7a1
mmaslan <marek.maslany1@gmail.com>
2022-12-30 18:30:06 +0100

    Update

--- Day 11 - 20/Day14/higher_lower.py
+++ Day 11 - 20/Day14/higher_lower.py
@@ -45,5 +45,20 @@ def run_game():
 
             print(f"Against B: {format_data(account_b)}")
 
+            guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+            a_followers_count = account_a["follower_count"]
+            b_followers_count = account_b["follower_count"]
+            is_correct = check_answer(guess, a_followers_count, b_followers_count)
+
+            clear()
+
+            print(logo)
+            if is_correct:
+                score += 1
+            else:
+                game_should_continue = False
+                print(f"Sorry, that's wrong. Final score: {score}")
+
+
 
 run_game()
