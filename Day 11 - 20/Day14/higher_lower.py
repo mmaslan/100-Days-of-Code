019d3dc88c7ab5c868ef89600b2052a375e25f16
mmaslan <marek.maslany1@gmail.com>
2022-12-30 17:55:54 +0100

    Day 14

--- Day 11 - 20/Day14/higher_lower.py
+++ Day 11 - 20/Day14/higher_lower.py
@@ -1,9 +1,21 @@
 
 import random
-from higher_lower_art import logo
+from higher_lower_art import logo, vs
 from game_data import data
+
+
+def random_data():
+    return random.choice(data)
 
 
 def run_game():
 
+
+
     print(logo)
+
+    print(f"Compare A: {name}, {description}, from {country}")
+
+    print(vs)
+
+    print(f"Against B: {name}, {description}, from {country}")
