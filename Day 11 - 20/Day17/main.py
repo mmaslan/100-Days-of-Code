bdeab6e65fe78e2a437d1cbef45298d0d78b886a
mmaslan <marek.maslany1@gmail.com>
2023-01-02 15:23:53 +0100

    Update

--- Day 11 - 20/Day17/main.py
+++ Day 11 - 20/Day17/main.py
@@ -1,5 +1,6 @@
 from question_model import Question
 from data import question_data
+from quiz_brain import QuizBrain
 
 
 question_bank = []
@@ -8,3 +9,7 @@ for question in question_data:
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
+
+quiz = QuizBrain(question_bank)
+quiz.next_question()
---
+++ Day 11 - 20/Day17/quiz_brain.py
@@ -0,0 +1,11 @@
+
+
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.question_list = q_list
+
+    def next_question(self):
+        current_question = self.question_list[self.question_number]
+        input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
