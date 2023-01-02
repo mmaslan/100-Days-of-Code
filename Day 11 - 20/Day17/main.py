b4059f9464467890cc568391b10dd1cdcfde8421
mmaslan <marek.maslany1@gmail.com>
2023-01-02 15:59:55 +0100

    Update

--- Day 11 - 20/Day17/main.py
+++ Day 11 - 20/Day17/main.py
@@ -12,4 +12,9 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You have completed the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
