2618e963a137b318d568ccc22953481dfb622f9d
mmaslan <marek.maslany1@gmail.com>
2023-01-03 15:57:04 +0100

    Update

--- Day 11 - 20/Day18/main.py
+++ Day 11 - 20/Day18/main.py
@@ -1,11 +1,16 @@
 
-from turtle import Turtle
+import turtle as turtle_module
 import colorgram
 import random
 
-tim = Turtle()
 
+turtle_module.colormode(255)
+tim = turtle_module.Turtle()
+tim.speed('fastest')
+tim.penup()
+tim.hideturtle()
 
+number_of_dots = 100
 rgb_colors = []
 colors = colorgram.extract('image.jpg', 30)
 for color in colors:
@@ -15,3 +20,18 @@ for color in colors:
     new_color = (r, g, b)
     rgb_colors.append(new_color)
 
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(rgb_colors))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+screen = turtle_module.Screen()
+screen.exitonclick()
