b7aeceec9fe587b83485d5dd823b2d53f9bfaca3
mmaslan <marek.maslany1@gmail.com>
2023-01-03 15:46:58 +0100

    Day 18

---
+++ Day 11 - 20/Day18/main.py
@@ -0,0 +1,17 @@
+
+from turtle import Turtle
+import colorgram
+import random
+
+tim = Turtle()
+
+
+rgb_colors = []
+colors = colorgram.extract('image.jpg', 30)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    new_color = (r, g, b)
+    rgb_colors.append(new_color)
+
