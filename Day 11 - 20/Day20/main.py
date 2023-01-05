44b5c1a75d779d9729f8790d2f88f4119a39d5ad
mmaslan <marek.maslany1@gmail.com>
2023-01-05 19:36:07 +0100

    Update

--- Day 11 - 20/Day20/main.py
+++ Day 11 - 20/Day20/main.py
@@ -1,7 +1,7 @@
 
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
-import random
 
 
 screen = Screen()
@@ -10,33 +10,16 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
+snake = Snake()
+
 game_is_on = True
 score = 0
 segments = []
 food_position = []
 
-
 starting_position = [(0, 0),(-20, 0),(-40, 0)]
-snake = Turtle("square")
-snake.color("white")
 
-
-for position in starting_position:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
 
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
-    for seg_num in range(len(segments)):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
-
-
-screen.exitonclick()
