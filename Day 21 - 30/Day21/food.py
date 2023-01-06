bbeca52c3ab6df492dcca811eb6ad0a602b0049e
mmaslan <marek.maslany1@gmail.com>
2023-01-06 13:43:35 +0100

    Update

--- Day 21 - 30/Day21/food.py
+++ Day 21 - 30/Day21/food.py
@@ -3,7 +3,7 @@ from turtle import Turtle
 import random
 
 
-class Food(Turtle)
+class Food(Turtle):
 
     def __init__(self):
         super().__init__()
@@ -12,6 +12,9 @@ class Food(Turtle)
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("blue")
         self.speed("fastest")
+        self.refresh()
+
+    def refresh(self):
         random_x = random.randint(-280, 280)
         random_y = random.randint(-280, 280)
         self.goto(random_x, random_y)
--- Day 21 - 30/Day21/main.py
+++ Day 21 - 30/Day21/main.py
@@ -24,7 +24,9 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 screen.exitonclick()
