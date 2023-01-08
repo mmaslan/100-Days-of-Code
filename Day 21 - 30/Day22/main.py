f2ad02415689b533736ce07cccc4f2cf2d3f0dce
mmaslan <marek.maslany1@gmail.com>
2023-01-08 14:03:43 +0100

    Update

--- Day 21 - 30/Day22/main.py
+++ Day 21 - 30/Day22/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+
 
 screen.listen()
 screen.onkey(r_paddle.go_up(), "Up")
@@ -31,6 +33,14 @@ while game_is_on:
 
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.ycor() > -320:
         ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        r_score += 1
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        l_score += 1
 
 
 screen.exitonclick()
