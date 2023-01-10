2bf501c868ad5682c56b8b0ddba4c31254cf11cd
mmaslan <marek.maslany1@gmail.com>
2023-01-10 21:59:02 +0100

    Day 24 and 23

---
+++ Day 21 - 30/Day24/Snake_Update/scoreboard.py
@@ -0,0 +1,36 @@
+
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = "Courier", 18, "normal"
+
+
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.high_score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def reset_scoreboard(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
