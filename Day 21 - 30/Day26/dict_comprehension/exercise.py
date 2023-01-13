53975efc35fc3f588c1393f9b2323b762d0e0605
mmaslan <marek.maslany1@gmail.com>
2023-01-13 23:31:01 +0100

    Update

---
+++ Day 21 - 30/Day26/dict_comprehension/exercise.py
@@ -0,0 +1,31 @@
+
+# import random
+#
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+#
+# student_score = {student:random.randint(1, 100) for student in names}
+# print(student_score)
+#
+# passed_student = {student:score for (student, score) in student_score.items() if score >= 60}
+# print(passed_student)
+
+
+# sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+# result = {word:len(word) for word in sentence.split()}
+#
+# print(result)
+
+# weather_c = {
+#     "Monday": 12,
+#     "Tuesday": 14,
+#     "Wednesday": 15,
+#     "Thursday": 14,
+#     "Friday": 21,
+#     "Saturday": 22,
+#     "Sunday": 24,
+# }
+#
+# weather_f = {day:((temp * 9/5) + 32) for (day, temp) in weather_c.items()}
+#
+# print(weather_f)
+
