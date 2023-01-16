929816d086005260e718ea6b31a6270590f29a33
mmaslan <marek.maslany1@gmail.com>
2023-01-16 21:45:11 +0100

    Update

--- Day 21 - 30/Day28/main.py
+++ Day 21 - 30/Day28/main.py
@@ -1,4 +1,3 @@
-
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -11,23 +10,43 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
-# ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER RESET ------------------------------- #
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def reset():
+    pass
+
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+def start():
+    pass
+
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50)
+window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
-canvas.create_image(103, 112, image=tomato_img)
-canvas.create_text()
-canvas.pack()
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
 
+button_start = Button(text="Start", highlightthickness=0)
+button_start.grid(column=0, row=2)
+
+button_reset = Button(text="Reset", highlightthickness=0)
+button_reset.grid(column=2, row=2)
+
+check_marks = Label(text="✔️", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
