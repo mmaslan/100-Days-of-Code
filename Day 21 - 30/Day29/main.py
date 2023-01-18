c00ccbab1b61b89856c64a53a4b72010470d4ce9
mmaslan <marek.maslany1@gmail.com>
2023-01-18 17:09:34 +0100

    Update

--- Day 21 - 30/Day29/main.py
+++ Day 21 - 30/Day29/main.py
@@ -4,27 +4,48 @@ from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
+def generate():
+    pass
+
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+
+
+def add():
+    pass
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=100, pady=50)
+window.config(padx=50, pady=50)
 
-canvas = Canvas(width=200, height=224, highlightthickness=0)
+canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
-canvas.create_image(100, 112, image=logo_img)
-canvas.grid(column=0, row=0)
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)
 
-web_label = Label(text="Website:")
-web_label.grid(column=0, row=1)
 
-user_label = Label(text="Email/Username:")
-user_label.grid(column=0, row=2)
+website_label = Label(text="Website:")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username:")
+email_label.grid(row=2, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
 
-pass_label = Label(text="Password:")
-pass_label.grid(column=0, row=3)
 
+web_entry = Entry(width=35)
+web_entry.grid(row=1, column=1, columnspan=2)
+web_entry.focus()
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "xxx@gmail.com")
+pass_entry = Entry(width=21)
+pass_entry.grid(row=3, column=1)
+
+
+gen_button = Button(text="Generate Password")
+gen_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
