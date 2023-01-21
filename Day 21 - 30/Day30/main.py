e7513e9d7de7ca5356fa595a1fc50a0adcd636b4
mmaslan <marek.maslany1@gmail.com>
2023-01-21 19:25:28 +0100

    Update

--- Day 21 - 30/Day30/main.py
+++ Day 21 - 30/Day30/main.py
@@ -1,15 +1,15 @@
-
+import json
 from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import string
 import pyperclip
+import json
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 def generate():
-
     letters = list(string.ascii_letters)
     numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
     symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -31,16 +31,29 @@ def add():
     website = web_entry.get()
     email = email_entry.get()
     password = pass_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password
+        }
+    }
 
     if len(website) == 0 or len(password) == 0:
         messagebox.showinfo(title="Error", text="Perhaps you typed wrong website or password")
     else:
-        is_ok = messagebox.askokcancel(title=website, message="Data uploaded to text_file")
-        if is_ok:
-            with open("data.txt", "a") as data_file:
-                data_file.write(f"Website: {website} / email: {email} / password: {password}")
-                web_entry.delete(0, END)
-                pass_entry.delete(0, END)
+        try:
+            with open("data.json", "r") as data_file:
+                data = json.load(data_file)
+        except FileNotFoundError:
+            with open("data.json", "w") as data_file:
+                json.dump(new_data, data_file, indent=4)
+        else:
+            data.update(new_data)
+            with open("data.json", "r") as data_file:
+                json.dump(data, data_file, indent=4)
+        finally:
+            web_entry.delete(0, END)
+            pass_entry.delete(0, END)
 
 
 window = Tk()
@@ -52,14 +65,12 @@ logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
-
 website_label = Label(text="Website:")
 website_label.grid(row=1, column=0)
 email_label = Label(text="Email/Username:")
 email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
-
 
 web_entry = Entry(width=35)
 web_entry.grid(row=1, column=1, columnspan=2)
@@ -69,7 +80,6 @@ email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, "xxx@gmail.com")
 pass_entry = Entry(width=21)
 pass_entry.grid(row=3, column=1)
-
 
 gen_button = Button(text="Generate Password", command=generate)
 gen_button.grid(row=3, column=2)
