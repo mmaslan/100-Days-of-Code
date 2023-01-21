ba22a662a58d3ecf0c3bfa48702871aa7a749636
mmaslan <marek.maslany1@gmail.com>
2023-01-21 19:29:40 +0100

    Update

--- Day 21 - 30/Day30/main.py
+++ Day 21 - 30/Day30/main.py
@@ -56,6 +56,10 @@ def add():
             pass_entry.delete(0, END)
 
 
+def find_password:
+    pass
+
+
 window = Tk()
 window.title("Password Manager")
 window.config(padx=50, pady=50)
@@ -72,7 +76,7 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
-web_entry = Entry(width=35)
+web_entry = Entry(width=21)
 web_entry.grid(row=1, column=1, columnspan=2)
 web_entry.focus()
 email_entry = Entry(width=35)
@@ -81,6 +85,8 @@ email_entry.insert(0, "xxx@gmail.com")
 pass_entry = Entry(width=21)
 pass_entry.grid(row=3, column=1)
 
+search_buttons = Button(text="Search", width=13, command=find_password)
+search_buttons.grid(row=1, column=2)
 gen_button = Button(text="Generate Password", command=generate)
 gen_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=36, command=add)
