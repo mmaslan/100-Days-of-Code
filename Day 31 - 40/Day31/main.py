65c6468cf7459059bd0eb80e2bc56c36d54c1dbb
mmaslan <marek.maslany1@gmail.com>
2023-01-22 21:53:18 +0100

    Update

--- Day 31 - 40/Day31/main.py
+++ Day 31 - 40/Day31/main.py
@@ -5,17 +5,25 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
 window.title("Flash Card App")
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
 right_image = PhotoImage(file="images/right.png")
 wrong_image = PhotoImage(file="images/wrong.png")
 
-canvas = Canvas(height=800, width=526)
+canvas = Canvas(width=800, height=526)
+card_front_image = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_image)
+canvas.create_text(400, 150, text="Title", font=("Arial", 40, "italic"))
+canvas.create_text(400, 263, text="text", font=("Arial", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0, row=0, columnspan=2)
 
-wrong_buttons = Button(image=wrong_image)
-wrong_buttons.grid(row=1, column=1)
-right_button = Button(image=right_image)
-right_button.grid(row=1, column=2)
+cross_image = PhotoImage
+
+wrong_buttons = Button(image=wrong_image, highlightthickness=0)
+wrong_buttons.grid(row=1, column=0)
+right_button = Button(image=right_image, highlightthickness=0)
+right_button.grid(row=1, column=1)
 
 
 window.mainloop()
