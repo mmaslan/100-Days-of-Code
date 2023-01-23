30047623d48517458680e4fc7a0b66bc17b94ff7
mmaslan <marek.maslany1@gmail.com>
2023-01-23 22:19:39 +0100

    Update

--- Day 31 - 40/Day31/main.py
+++ Day 31 - 40/Day31/main.py
@@ -5,10 +5,16 @@ import random
 
 BACKGROUND_COLOR = "#B1DDC6"
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
 current_card = {}
 words_to_learn = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
 
 
 def random_text():
@@ -25,6 +31,14 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
     canvas.itemconfig(card_background, image=card_back_image)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    to_learn_data = pandas.DataFrame(to_learn)
+    to_learn_data.to_csv("data/words_to_learn.csv", index=False)
+
+    random_text()
 
 
 window = Tk()
