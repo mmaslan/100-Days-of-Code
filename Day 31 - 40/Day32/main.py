976023953ff7bae11fba6b0567e253393c968674
mmaslan <marek.maslany1@gmail.com>
2023-01-24 17:04:28 +0100

    Update

--- Day 31 - 40/Day32/main.py
+++ Day 31 - 40/Day32/main.py
@@ -4,8 +4,15 @@ import smtplib
 my_email = "generic@gmail.com"
 password = "genericpassword"
 
-connections = smtplib.SMTP("smtp.gmail.com")
-connections.starttls()
-connections.login(user=my_email, password=password)
-connections.sendmail(from_addr=my_email, to_addrs="generic@yahoo.com", msg="Hello")
-connections.close()
+
+with smtplib.SMTP("smtp.gmail.com") as connections:
+    connections.starttls()
+    connections.login(user=my_email, password=password)
+    connections.sendmail(
+        from_addr=my_email,
+        to_addrs="generic@yahoo.com",
+        msg="Subject:Hello\n\nThis is the body of the email."
+    )
+    connections.close()
+
+import datetime
