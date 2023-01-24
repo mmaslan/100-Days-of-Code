d65541b50c69c197835a05e7543757f2bcc85984
mmaslan <marek.maslany1@gmail.com>
2023-01-25 00:07:49 +0100

    Update

--- Day 31 - 40/Day32/main.py
+++ Day 31 - 40/Day32/main.py
@@ -1,18 +1,24 @@
 import smtplib
+import datetime as dt
+import random
 
+now = dt.datetime.now()
+weekday = now.weekday()
 
 my_email = "generic@gmail.com"
 password = "genericpassword"
 
 
-with smtplib.SMTP("smtp.gmail.com") as connections:
-    connections.starttls()
-    connections.login(user=my_email, password=password)
-    connections.sendmail(
-        from_addr=my_email,
-        to_addrs="generic@yahoo.com",
-        msg="Subject:Hello\n\nThis is the body of the email."
-    )
-    connections.close()
+random_quote = random.choice(list(open("quotes.txt", "r")))
 
-import datetime
+if weekday == 0:
+    with smtplib.SMTP("smtp.gmail.com") as connections:
+        connections.starttls()
+        connections.login(user=my_email, password=password)
+        connections.sendmail(
+            from_addr=my_email,
+            to_addrs="generic@yahoo.com",
+            msg=f"Subject:Hello\n\n{random_quote}."
+        )
+        connections.close()
+
