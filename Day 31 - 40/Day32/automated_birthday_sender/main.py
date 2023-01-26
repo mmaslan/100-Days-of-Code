8ca9627b71b9ff347650343c34d9801e145240af
mmaslan <marek.maslany1@gmail.com>
2023-01-27 00:18:13 +0100

    Update

--- Day 31 - 40/Day32/automated_birthday_sender/main.py
+++ Day 31 - 40/Day32/automated_birthday_sender/main.py
@@ -1,11 +1,19 @@
 import pandas
 import smtplib
+import random
+import datetime as dt
 
-
+today_month = dt.datetime.month
+today_day = dt.datetime.weekday
 today = (today_month, today_day)
 
 
 b_data = pandas.read_csv("birthdays.csv")
-birthday_dict = b_data.to_dict()
+data_dict = b_data.to_dict()
+
+birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in b_data.iterrows()}
 
 print(birthday_dict)
+
+# if (today_month, today_day) in birthdays_dict:
+#     pass
