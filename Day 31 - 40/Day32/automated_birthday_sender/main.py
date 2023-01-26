b419f39b27a9b72ca6c502e79856853e4cc65cf1
mmaslan <marek.maslany1@gmail.com>
2023-01-27 00:50:44 +0100

    Update

--- Day 31 - 40/Day32/automated_birthday_sender/main.py
+++ Day 31 - 40/Day32/automated_birthday_sender/main.py
@@ -1,19 +1,20 @@
 import pandas
 import smtplib
 import random
-import datetime as dt
+from datetime import datetime
 
-today_month = dt.datetime.month
-today_day = dt.datetime.weekday
-today = (today_month, today_day)
+
+today = datetime.now()
+today_tuple = (today.month, today.day)
 
 
 b_data = pandas.read_csv("birthdays.csv")
 data_dict = b_data.to_dict()
 
 birthday_dict = {(data_row["month"], data_row["day"]): data_row for (index, data_row) in b_data.iterrows()}
-
 print(birthday_dict)
 
-# if (today_month, today_day) in birthdays_dict:
-#     pass
+if today_tuple in birthday_dict:
+    birthday_person = birthday_dict[today]
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as file:
