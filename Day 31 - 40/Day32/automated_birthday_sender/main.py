f5da34b29b1483111f6ae16fd9a6fe68f555a7c2
mmaslan <marek.maslany1@gmail.com>
2023-01-28 00:02:27 +0100

    Update

--- Day 31 - 40/Day32/automated_birthday_sender/main.py
+++ Day 31 - 40/Day32/automated_birthday_sender/main.py
@@ -7,6 +7,8 @@ from datetime import datetime
 today = datetime.now()
 today_tuple = (today.month, today.day)
 
+MY_EMAIL = "YOUR EMAIL"
+MY_PASSWORD = "YOUR PASSWORD"
 
 b_data = pandas.read_csv("birthdays.csv")
 data_dict = b_data.to_dict()
@@ -18,3 +20,15 @@ if today_tuple in birthday_dict:
     birthday_person = birthday_dict[today]
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
     with open(file_path) as file:
+        contents = file.read()
+        contents = contents.replace("[NAME]", birthday_person["name"])
+
+    with smtplib.SMTP("smtp.gmail.com") as connections:
+        connections.starttls()
+        connections.login(MY_EMAIL, MY_PASSWORD)
+        connections.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!\n\n{contents}"
+        )
+
