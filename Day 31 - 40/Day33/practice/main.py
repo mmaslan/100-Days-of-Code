e6a47ac81ace0df50effe032577f69262cc02612
mmaslan <marek.maslany1@gmail.com>
2023-01-30 23:28:04 +0100

    ISS finder

--- Day 31 - 40/Day33/practice/main.py
+++ Day 31 - 40/Day33/practice/main.py
@@ -29,12 +29,12 @@ parameters = {
 response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response.raise_for_status()
 data = response.json()
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
 print(sunrise)
 print(sunset)
 
 time_now = datetime.datetime.now()
 
-print(time_now)
+print(time_now.hour)
