4f6aa3b998d4098cc34c35b44263c90008bc78a6
mmaslan <marek.maslany1@gmail.com>
2023-02-01 22:51:19 +0100

    Update

--- Day 31 - 40/Day34/quizzler/ui.py
+++ Day 31 - 40/Day34/quizzler/ui.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 
 THEME_COLOR = "#375362"
@@ -7,7 +8,8 @@ FONT = ("Arial", 20, "italic")
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -19,6 +21,7 @@ class QuizInterface:
         self.question_text = self.canvas.create_text(
             150,
             125,
+            width=280,
             text="Some question text",
             fill=THEME_COLOR,
             font=FONT
@@ -33,7 +36,14 @@ class QuizInterface:
         self.true_button = Button(image=self.false_img, highlightthickness=0)
         self.true_button.grid(row=2, column=1)
 
+        self.get_new_question()
+
         self.window.mainloop()
+
+    def get_new_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
+
 
 
 
