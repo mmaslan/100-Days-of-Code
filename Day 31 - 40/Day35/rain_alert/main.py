84a22e6d82f2c4874e930c1e9f4c6e04b0520339
mmaslan <marek.maslany1@gmail.com>
2023-02-03 23:52:21 +0100

    Update

--- Day 31 - 40/Day35/rain_alert/main.py
+++ Day 31 - 40/Day35/rain_alert/main.py
@@ -2,17 +2,19 @@ import json
 import requests
 import datetime
 
-OMW_Endpoint = 'http://api.openweathermap.org/data/2.5/weather?'
+OMW_Endpoint = 'http://api.openweathermap.org/data/2.5/onecall'
 api_key = '588237482f885cef420a4fa9796cd1b9'
 
 weather_params = {
     'lat': 54.356030,
     'lon': 18.646120,
-    'appid': api_key
+    'appid': api_key,
+    'exclude': 'currently,minutely,daily',
 }
 
 response = requests.get(OMW_Endpoint, params=weather_params)
 response.raise_for_status()
-data = response.json()
-# weather_data = data["weather"][""]
-# print(weather_data)
+weather_data = response.json()
+weather_slice = weather_data["hourly"][:12]
+for hour_data in weather_slice:
+    print(hour_data["weather"])
