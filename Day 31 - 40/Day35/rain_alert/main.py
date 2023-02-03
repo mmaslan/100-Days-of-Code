6621d89e3b295868210762474eda58ce895592c1
mmaslan <marek.maslany1@gmail.com>
2023-02-04 00:55:00 +0100

    Update

--- Day 31 - 40/Day35/rain_alert/main.py
+++ Day 31 - 40/Day35/rain_alert/main.py
@@ -16,5 +16,13 @@ response = requests.get(OMW_Endpoint, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 weather_slice = weather_data["hourly"][:12]
+
+will_rain = False
+
 for hour_data in weather_slice:
-    print(hour_data["weather"])
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an umbrella.")
