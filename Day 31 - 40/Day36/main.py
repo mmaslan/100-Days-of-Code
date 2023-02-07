a07a5ebe2d763c7811424bb3bd895512c0c6d3f5
mmaslan <marek.maslany1@gmail.com>
2023-02-07 16:22:31 +0100

    Update

--- Day 31 - 40/Day36/main.py
+++ Day 31 - 40/Day36/main.py
@@ -1,7 +1,6 @@
 import os
 import requests
 
-api_key = os.environ['NEWSAPI']
 
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
@@ -9,14 +8,20 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
+STOCK_API = '2FSVCM3NLDZ5HCE6'
+
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
     "symbol": STOCK_NAME,
-    "apikey": api_key,
+    "apikey": STOCK_API,
 }
 
-response = requests.get(NEWS_ENDPOINT, params=stock_params)
-stock_data = response.json()
-print(stock_data)
+response = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = response.json()["Time Series (Daily)"]
+data_list = [value for (key, value) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
 
