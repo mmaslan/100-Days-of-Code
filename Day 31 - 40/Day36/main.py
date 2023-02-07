b7fec27e422809c855af67d9b171525e57a54575
mmaslan <marek.maslany1@gmail.com>
2023-02-07 16:31:56 +0100

    Update

--- Day 31 - 40/Day36/main.py
+++ Day 31 - 40/Day36/main.py
@@ -8,7 +8,7 @@ COMPANY_NAME = "Tesla Inc"
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
-STOCK_API = '2FSVCM3NLDZ5HCE6'
+STOCK_API = os.environ['ALPHA_VANTAGE_API']
 
 
 stock_params = {
@@ -22,6 +22,6 @@ data = response.json()["Time Series (Daily)"]
 data_list = [value for (key, value) in data.items()]
 yesterday_data = data_list[0]
 yesterday_closing_price = yesterday_data["4. close"]
-print(yesterday_closing_price)
 
-
+day_before_yesterday_data = data_list[1]
+day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
