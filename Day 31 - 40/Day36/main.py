5eec16284681567712fb29135a7b0f36b45fdfa0
mmaslan <marek.maslany1@gmail.com>
2023-02-07 23:53:15 +0100

    Update

--- Day 31 - 40/Day36/main.py
+++ Day 31 - 40/Day36/main.py
@@ -1,7 +1,6 @@
 import os
 import requests
 
-
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
@@ -9,7 +8,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API = os.environ['ALPHA_VANTAGE_API']
-
+NEWS_API_KEY = os.environ['NEWSAPI']
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -25,3 +24,14 @@ yesterday_closing_price = yesterday_data["4. close"]
 
 day_before_yesterday_data = data_list[1]
 day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
+
+difference = abs(float(yesterday_closing_price)) - float(day_before_yesterday_closing_price)
+
+diff_percent = difference / float(yesterday_closing_price) * 100
+
+if diff_percent > 4:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
